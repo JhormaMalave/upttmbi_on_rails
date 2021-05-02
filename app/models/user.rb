78540def7a4d54df2d7a50fc4aec930927fc1d2f
JhormaMalave <jhormamalave99@gmail.com>
2021-05-01 23:43:22 -0400

    Configure role model for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :roles, through: :user_role
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
