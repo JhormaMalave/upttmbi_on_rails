132e41362bf48cedcd368e696fa28c1f2b7fb0ff
JhormaMalave <jhormamalave99@gmail.com>
2021-05-07 21:39:16 -0400

    Add edit action in staff teacher

--- app/controllers/staff/teachers_controller.rb
+++ app/controllers/staff/teachers_controller.rb
@@ -1,5 +1,5 @@
 class Staff::TeachersController < ApplicationController
-  before_action :set_teacher, only: %i[ show ]
+  before_action :set_teacher, only: %i[ show edit update ]
 
   # GET /staff/teachers or /staff/teachers.json
   def index
@@ -9,6 +9,10 @@ class Staff::TeachersController < ApplicationController
   # GET /staff/teachers/new
   def new
     @teacher = Teacher.new
+  end
+
+  # GET /staff/teachers/1/edit
+  def edit
   end
 
   # POST /staff/teachers or /staff/teachers.json
@@ -21,6 +25,19 @@ class Staff::TeachersController < ApplicationController
         format.json { render :show, status: :created, location: @teacher }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @teacher.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /staff/teachers/1 or /staff/teachers/1.json
+  def update
+    respond_to do |format|
+      if @teacher.update(teacher_params)
+        format.html { redirect_to polymorphic_path([:staff, @teacher]), notice: "El docente se actualizo correctamente." }
+        format.json { render :show, status: :ok, location: @teacher }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @teacher.errors, status: :unprocessable_entity }
       end
     end
