1a4431d02b32e8b2bf717dae9327995cf36a0867
JhormaMalave <jhormamalave99@gmail.com>
2021-05-23 17:44:45 -0400

    Add user controller actions

--- app/controllers/staff/users_controller.rb
+++ app/controllers/staff/users_controller.rb
@@ -1,5 +1,47 @@
 class Staff::UsersController < Staff::StaffController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  # GET /staff/users
   def index
     @users = User.all
   end
+
+  # GET /staff/users/1
+  def show
+  end
+
+  # GET /staff/users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /staff/user/1/edit
+  def edit
+  end
+
+  # POST /staff/users or /staff/users.json
+  def create
+    @user = user.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to staff_user_path(@user), notice: "La asignatura fue creada exitosamente." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = user.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
